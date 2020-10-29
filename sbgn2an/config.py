3668173c8feceeac1cc761fe38413cfc6b450793
Adrien Rougny <adrienrougny@gmail.com>
2020-10-29 15:15:01 +0900

    cleaned code and removed unnecessary files

--- sbgn2an/config.py
+++ sbgn2an/config.py
@@ -1,6 +1,3 @@
 import os.path
-MAX_FILE = os.path.join(os.path.dirname(__file__), "max.asp")
-SETS_FILE = os.path.join(os.path.dirname(__file__), "sets.asp")
 STORIES_FILE = os.path.join(os.path.dirname(__file__), "stories.asp")
-SEEDS_FILE = os.path.join(os.path.dirname(__file__), "seeds.asp")
 INI_FILE = os.path.join(os.path.dirname(__file__), "ini.asp")
