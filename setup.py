b2f3b897d242bd12211eeda23ce8b35a97432bbf
Loïc Paulevé <loic.pauleve@labri.fr>
2020-10-30 21:31:26 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='sbgn2an',
       version='0.1',
-      description='Library for computing initial states and stories of a Process Description map'
+      description='Library for computing initial states and stories of a Process Description map',
       author="Adrien Rougny, Loïc Paulevé",
       author_email="adrienrougny@gmail.com, loic.pauleve@lri.fr",
       packages=['sbgn2an'],
